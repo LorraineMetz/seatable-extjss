3c240bf060bbccc7d3760591dfd17458910b475f
LorraineMetz <lorraine1314@outlook.com>
2024-09-13 00:14:56 +0800

    1.0.2

--- extjss.js
+++ extjss.js
@@ -1,84 +1,258 @@
-class Table {
-  constructor(table_name) {
-    if (table_name) {
-      this._tableName = table_name;
-    } else {
-      this._tableName = base.getActiveTable().name;
+(function () {
+    var base = window['base'];
+    var app = window['app'];
+    function die(errMsg) {
+        alert(errMsg);
+        throw new Error(errMsg);
     }
-
-    this._tableObj = base.getTableByName(this._tableName);
-    return this;
-  }
-
-  view(view_name) {
-    if (view_name) {
-      this._viewName = view_name;
-    } else {
-      this._viewName = base.getActiveView();
+    function inject(func) {
+        var name = func['name'];
+        window[name] = func;
     }
-
-    this._viewObj = base.getViewByName(this._tableName, this._viewName);
-    return this;
-  }
-
-  rows(filter) {
-    if (filter) {
-      if (typeof filter == "function") {
-        this._rowsObj = base
-          .getRows(this._tableName, this._viewName)
-          .filter(filter);
-      } else if (typeof filter == "string") {
-        this._rowsObj = base.filter(this._tableName, this._viewName, filter);
-      }
-    } else {
-      this._rowsObj = base.getRows(this._tableName, this._viewName);
+    function extjss() {
+        var LIB_VERSION = "1.0.2";
+        console.warn("extjss v".concat(LIB_VERSION, " loaded"));
     }
-    return this;
-  }
-
-  column(column_name) {
-    if (column_name) {
-      this._columnName = column_name;
-    }else {
-      alert("脚本执行失败，原因是未指定字段名称“.column(column_name)”");
+    inject(extjss);
+    function table(table_name) {
+        if (table_name === void 0) { table_name = undefined; }
+        return new Table(table_name);
     }
-    return this;
-  }
-
-  exec(func) {
-    var selectedRows = [],
-      updateRows = [];
-    this._rowsObj.forEach((row) => {
-      selectedRows.push(row);
-      updateRows.push({
-        [this._columnName]: func(row),
-      });
-    });
-    base.modifyRows(this._tableObj, selectedRows, updateRows);
-  }
-}
-
-window.extjss = (function () {
-  const FUNC_VERSION = "1.0.1";
-  console.warn("extjss loaded");
-
-  window.table = function (table_name) {
-    return new Table(table_name);
-  };
-
-  window.view = function (view_name) {
-    return table().view(view_name);
-  };
-
-  window.rows = function (filter) {
-    return view().rows(filter);
-  };
-
-  window.column = function (column_name) {
-    return rows().column(column_name);
-  };
-
-  return {
-    version: FUNC_VERSION,
-  };
+    inject(table);
+    function view(view_name) {
+        if (view_name === void 0) { view_name = undefined; }
+        return new View(table(), view_name);
+    }
+    inject(view);
+    function rows(filter) {
+        if (filter === void 0) { filter = undefined; }
+        return new RowsFilter(view(), filter);
+    }
+    inject(rows);
+    function row() {
+        return new Row();
+    }
+    inject(row);
+    function column(column_name) {
+        return new ColumnModifier(rows(), column_name);
+    }
+    inject(column);
+    var ColumnModifier = /** @class */ (function () {
+        function ColumnModifier(rows_filter, column_name) {
+            this.rowsFilter = rows_filter;
+            if (column_name) {
+                this._columnName = column_name;
+            }
+            else {
+                die("脚本执行失败，原因是未指定字段名称“.column(column_name)”");
+            }
+        }
+        ColumnModifier.prototype.exec = function (func) {
+            var _this = this;
+            var selectedRows = [], updateRows = [];
+            this.rowsFilter.rows.forEach(function (row) {
+                var _a;
+                try {
+                    var result = func(row);
+                    selectedRows.push(row);
+                    updateRows.push((_a = {},
+                        _a[_this._columnName] = result,
+                        _a));
+                }
+                catch (error) {
+                    return;
+                }
+            });
+            base.modifyRows(this.rowsFilter.table.name, selectedRows, updateRows);
+        };
+        ColumnModifier.prototype.ref = function (raw_filter, func, operation, sourceTableName) {
+            if (operation === void 0) { operation = undefined; }
+            if (sourceTableName === void 0) { sourceTableName = undefined; }
+            if (!sourceTableName) {
+                sourceTableName = this.rowsFilter.table.name;
+            }
+            var sourceTable = base.getTableByName(sourceTableName);
+            if (!sourceTable) {
+                die("\u8868\u3010".concat(sourceTableName, "\u3011\u4E0D\u5B58\u5728"));
+            }
+            var sourceRows = sourceTable.rows.map(function (r) { return base.getRowById(sourceTableName, r['_id']); });
+            this.exec(function (r) {
+                var results = raw_filter(r, sourceRows);
+                if (operation == "last") {
+                    return func(r, results[results.length - 1]);
+                }
+                else {
+                    return func(r, results[0]);
+                }
+            });
+        };
+        //calc_option 可选值
+        ColumnModifier.prototype.map = function (index_column, map_key, result_column, operation) {
+            if (operation === void 0) { operation = undefined; }
+            var sourceTableName = this.rowsFilter.table.name;
+            var sourceColumnName = map_key;
+            var maps = map_key.split('/');
+            if (maps.length > 1) {
+                sourceTableName = maps[0];
+                sourceColumnName = maps[1];
+            }
+            var sourceTable = base.getTableByName(sourceTableName);
+            if (!sourceTable) {
+                die("\u8868\u3010".concat(sourceTableName, "\u3011\u4E0D\u5B58\u5728"));
+            }
+            var sourceRows = sourceTable.rows.map(function (r) { return base.getRowById(sourceTableName, r['_id']); });
+            this.exec(function (r) {
+                var results = sourceRows.filter(function (a) { return a[sourceColumnName] == r[index_column]; });
+                if (results.length == 0) {
+                    throw new Error();
+                }
+                switch (operation) {
+                    case "sum":
+                        return results.map(function (a) { return a[result_column] * 1; }).reduce(function (s, i) { return s + i; }, 0);
+                    case "avg":
+                        return results.map(function (a) { return a[result_column] * 1; }).reduce(function (s, i) { return s + i; }, 0) / results.length;
+                    case "count":
+                        return results.length;
+                    case "max":
+                        return Math.max.apply(Math, results.map(function (a) { return a[result_column] * 1; }));
+                    case "min":
+                        return Math.min.apply(Math, results.map(function (a) { return a[result_column] * 1; }));
+                    case "first":
+                        return results[0][result_column];
+                    case "last":
+                        return results[results.length - 1][result_column];
+                    default:
+                        return results[0][result_column];
+                }
+            });
+        };
+        return ColumnModifier;
+    }());
+    var RowsFilter = /** @class */ (function () {
+        function RowsFilter(view, row_filter) {
+            if (row_filter === void 0) { row_filter = undefined; }
+            this.rows = [];
+            var tableName = view.table.name;
+            var viewName = view.name;
+            this.table = view.table;
+            this.view = view;
+            if (row_filter) {
+                if (typeof row_filter == "function") {
+                    this.rows = base
+                        .getRows(tableName, viewName)
+                        .filter(row_filter);
+                }
+                else if (typeof row_filter == "string") {
+                    var result = base.filter(tableName, viewName, row_filter);
+                    if (result) {
+                        this.rows = result.all();
+                    }
+                }
+                else if (Array.isArray(row_filter)) {
+                    var qs_1;
+                    row_filter.forEach(function (f) {
+                        if (qs_1) {
+                            qs_1 = qs_1.filter(f);
+                        }
+                        else {
+                            qs_1 = base.filter(tableName, viewName, f);
+                        }
+                    });
+                    if (qs_1) {
+                        this.rows = qs_1.all();
+                    }
+                }
+            }
+            else {
+                this.rows = base.getRows(tableName, viewName);
+            }
+        }
+        RowsFilter.prototype.column = function (column_name) {
+            return new ColumnModifier(this, column_name);
+        };
+        RowsFilter.prototype.print = function (column_names) {
+            if (column_names === void 0) { column_names = undefined; }
+            if (column_names) {
+                if (typeof column_names == 'string') {
+                    this.rows.forEach(function (r) {
+                        console.log(r[column_names]);
+                    });
+                }
+                else if (Array.isArray(column_names)) {
+                    this.rows.forEach(function (r) {
+                        console.log(column_names.map(function (k) { return r[k]; }));
+                    });
+                }
+            }
+            else {
+                this.rows.forEach(function (r) {
+                    console.log(r);
+                });
+            }
+        };
+        return RowsFilter;
+    }());
+    var Row = /** @class */ (function () {
+        function Row() {
+            this.obj = app.getCurrentRow();
+            if (this.obj) {
+                this.id = this.obj._id;
+            }
+            else {
+                die('Row 对象只生效在当前行');
+            }
+        }
+        Row.prototype.column = function (column_name) {
+            var rowsFilter = rows();
+            rowsFilter.rows = [base.getRowById(rowsFilter.table.name, this.id)];
+            return new ColumnModifier(rowsFilter, column_name);
+        };
+        return Row;
+    }());
+    var View = /** @class */ (function () {
+        function View(table, view_name) {
+            if (view_name === void 0) { view_name = undefined; }
+            this.table = table;
+            if (view_name) {
+                this.obj = base.getViewByName(table.name, view_name);
+            }
+            else {
+                this.obj = base.getActiveView();
+            }
+            this.name = this.obj.name;
+        }
+        View.prototype.rows = function (row_filter) {
+            if (row_filter === void 0) { row_filter = undefined; }
+            return new RowsFilter(this, row_filter);
+        };
+        return View;
+    }());
+    var Table = /** @class */ (function () {
+        function Table(table_name) {
+            if (table_name === void 0) { table_name = undefined; }
+            if (table_name) {
+                this.obj = base.getTableByName(table_name);
+            }
+            else {
+                this.obj = base.getActiveTable();
+            }
+            this.name = this.obj.name;
+        }
+        Table.prototype.view = function (view_name) {
+            if (view_name === void 0) { view_name = undefined; }
+            return new View(this, view_name);
+        };
+        // lookupAndCopy
+        Table.prototype.map = function (fill_column, index_column, map_key, result_column) {
+            var maps = map_key.split('/');
+            var sourceTableName = this.name;
+            var sourceColumnName = map_key;
+            if (maps.length > 1) {
+                sourceTableName = maps[0];
+                sourceColumnName = maps[1];
+            }
+            base.utils.lookupAndCopy(this.name, fill_column, index_column, sourceTableName, result_column, sourceColumnName);
+        };
+        return Table;
+    }());
 })();
---
+++ extjss.ts
@@ -0,0 +1,310 @@
+declare namespace Types {
+    export type RowFilterFunction = (row: object) => boolean;
+    export type RowsFilterFunction = (row: object, rows: Array<object>) => Array<object>;
+    export type ColumnModifyFunction = (row: object) => any;
+    export type ColumnRefModifyFunction = (row: object, ds: object) => any;
+}
+
+
+(function () {
+    const base: any = window['base'];
+    const app: any = window['app'];
+
+    function die(errMsg) {
+        alert(errMsg);
+        throw new Error(errMsg);
+    }
+
+    function inject(func: Function): void {
+        const name = func['name'];
+        window[name] = func;
+    }
+
+    function extjss() {
+        const LIB_VERSION = "1.0.2";
+        console.warn(`extjss v${LIB_VERSION} loaded`);
+    }
+    inject(extjss);
+
+    function table(table_name: string | undefined = undefined): Table {
+        return new Table(table_name);
+    }
+    inject(table);
+
+    function view(view_name: string | undefined = undefined): View {
+        return new View(table(), view_name);
+    }
+    inject(view);
+
+    function rows(filter: Types.RowFilterFunction | string | Array<string> | undefined = undefined): RowsFilter {
+        return new RowsFilter(view(), filter);
+    }
+    inject(rows);
+
+    function row() {
+        return new Row();
+    }
+    inject(row);
+
+    function column(column_name: string): ColumnModifier {
+        return new ColumnModifier(rows(), column_name);
+    }
+    inject(column);
+
+
+
+    class ColumnModifier {
+        public rowsFilter: RowsFilter;
+        private _columnName: string;
+
+        constructor(rows_filter: RowsFilter, column_name: string) {
+            this.rowsFilter = rows_filter;
+            if (column_name) {
+                this._columnName = column_name;
+            } else {
+                die("脚本执行失败，原因是未指定字段名称“.column(column_name)”");
+            }
+        }
+
+
+        public exec(func: Types.ColumnModifyFunction): void {
+            const selectedRows: Array<any> = [],
+                updateRows: Array<any> = [];
+            this.rowsFilter.rows.forEach((row) => {
+                try {
+                    const result = func(row);
+                    selectedRows.push(row);
+                    updateRows.push({
+                        [this._columnName]: result,
+                    });
+                } catch (error) {
+                    return;
+                }
+            });
+            base.modifyRows(this.rowsFilter.table.name, selectedRows, updateRows);
+        }
+
+        public ref(raw_filter: Types.RowsFilterFunction, func:Types.ColumnRefModifyFunction, 
+            operation: "last" | "first" | undefined = undefined,
+            sourceTableName : string|undefined = undefined) : void {
+            if (!sourceTableName) {
+                sourceTableName = this.rowsFilter.table.name;
+            }
+            const sourceTable = base.getTableByName(sourceTableName);
+            if (!sourceTable) {
+                die(`表【${sourceTableName}】不存在`);
+            }
+            const sourceRows = sourceTable.rows.map(r => base.getRowById(sourceTableName, r['_id']))
+
+            this.exec(r=> {
+                var results = raw_filter(r, sourceRows);
+                if (operation == "last") {
+                    return func(r, results[results.length - 1]);
+                }else {
+                    return func(r, results[0]);
+                }
+            });
+        }
+
+        //calc_option 可选值
+        public map(index_column: string, map_key: string, result_column: string,
+            operation: "sum" | "count" | "first" | "last" | "max" | "min" | "avg" | undefined = undefined): void {
+
+            let sourceTableName = this.rowsFilter.table.name;
+            let sourceColumnName = map_key;
+            const maps = map_key.split('/');
+            if (maps.length > 1) {
+                sourceTableName = maps[0];
+                sourceColumnName = maps[1];
+            }
+            const sourceTable = base.getTableByName(sourceTableName);
+            if (!sourceTable) {
+                die(`表【${sourceTableName}】不存在`);
+            }
+            const sourceRows = sourceTable.rows.map(r => base.getRowById(sourceTableName, r['_id']))
+
+            this.exec(r => {
+                var results = sourceRows.filter(a => a[sourceColumnName] == r[index_column]);
+                if (results.length == 0) {
+                    throw new Error();
+                }
+                switch (operation) {
+                    case "sum":
+                        return results.map(a => a[result_column] * 1).reduce((s, i) => s + i, 0);
+                    case "avg":
+                        return results.map(a => a[result_column] * 1).reduce((s, i) => s + i, 0) / results.length;
+                    case "count":
+                        return results.length;
+                    case "max":
+                        return Math.max(...results.map(a => a[result_column] * 1));
+                    case "min":
+                        return Math.min(...results.map(a => a[result_column] * 1));
+                    case "first":
+                        return results[0][result_column];
+                    case "last":
+                        return results[results.length - 1][result_column];
+                    default:
+                        return results[0][result_column];
+
+                }
+            });
+        }
+    }
+
+
+    class RowsFilter {
+        public table: Table;
+        public view: View;
+        public rows: Array<any> = [];
+
+        constructor(view: View, row_filter: Types.RowFilterFunction | string | Array<string> | undefined = undefined) {
+            const tableName = view.table.name;
+            const viewName = view.name;
+            this.table = view.table;
+            this.view = view;
+
+
+            if (row_filter) {
+                if (typeof row_filter == "function") {
+                    this.rows = base
+                        .getRows(tableName, viewName)
+                        .filter(row_filter);
+                } else if (typeof row_filter == "string") {
+                    const result = base.filter(tableName, viewName, row_filter);
+                    if (result) {
+                        this.rows = result.all();
+                    }
+                } else if (Array.isArray(row_filter)) {
+                    let qs;
+                    row_filter.forEach(f => {
+                        if (qs) {
+                            qs = qs.filter(f);
+                        } else {
+                            qs = base.filter(tableName, viewName, f);
+                        }
+                    })
+                    if (qs) {
+                        this.rows = qs.all();
+                    }
+                }
+            } else {
+                this.rows = base.getRows(tableName, viewName);
+            }
+        }
+
+
+        public column(column_name: string) {
+            return new ColumnModifier(this, column_name);
+        }
+
+        public print(column_names: string | Array<string> | undefined = undefined): void {
+            if (column_names) {
+                if (typeof column_names == 'string') {
+                    this.rows.forEach(r => {
+                        console.log(r[column_names]);
+                    })
+                } else if (Array.isArray(column_names)) {
+
+                    this.rows.forEach(r => {
+                        console.log(column_names.map(k => r[k]));
+                    })
+                }
+            } else {
+                this.rows.forEach(r => {
+                    console.log(r);
+                })
+            }
+
+        }
+    }
+
+    class Row {
+        public id: string;
+        public obj: any;
+
+        constructor() {
+            this.obj = app.getCurrentRow();
+            if (this.obj) {
+                this.id = this.obj._id;
+            } else {
+                die('Row 对象只生效在当前行');
+            }
+        }
+
+        public column(column_name: string) {
+            const rowsFilter = rows();
+            rowsFilter.rows = [base.getRowById(rowsFilter.table.name, this.id)];
+            return new ColumnModifier(rowsFilter, column_name);
+        }
+    }
+
+
+    class View {
+        public name: string;
+        public obj: any;
+
+        public table: Table;
+
+        constructor(table: Table, view_name: string | undefined = undefined) {
+            this.table = table;
+            if (view_name) {
+                this.obj = base.getViewByName(table.name, view_name);
+            } else {
+                this.obj = base.getActiveView();
+            }
+            this.name = this.obj.name;
+        }
+
+
+        public rows(row_filter: Types.RowFilterFunction | string | Array<string> | undefined = undefined): RowsFilter {
+            return new RowsFilter(this, row_filter);
+        }
+
+    }
+
+
+
+
+    class Table {
+        public name: string;
+        public obj: any;
+
+
+        constructor(table_name: string | undefined = undefined) {
+            if (table_name) {
+                this.obj = base.getTableByName(table_name);
+            } else {
+                this.obj = base.getActiveTable();
+            }
+
+            this.name = this.obj.name;
+        }
+
+        public view(view_name: string | undefined = undefined): View {
+            return new View(this, view_name);
+        }
+
+        // lookupAndCopy
+        public map(fill_column: string, index_column: string, map_key: string, result_column: string): void {
+            const maps = map_key.split('/');
+            let sourceTableName = this.name;
+            let sourceColumnName = map_key;
+            if (maps.length > 1) {
+                sourceTableName = maps[0];
+                sourceColumnName = maps[1];
+            }
+
+            base.utils.lookupAndCopy(
+                this.name, fill_column, index_column,
+                sourceTableName, result_column, sourceColumnName
+            );
+        }
+    }
+
+})();
+
+
+
+
+
+
