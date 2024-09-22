9d645b025846eb5a078da07385a3efb8b01d5f00
LorraineMetz <lorraine1314@outlook.com>
2024-09-22 19:04:57 +0800

    fix

--- src/split.ts
+++ src/split.ts
@@ -1,5 +1,4 @@
-// todo
-// 表拆分工具，链式调用法为：table().split({"表": "条件" or ()=>()}).exec(replace_duplicate)
+// [ ] 表拆分工具，链式调用法为：table().split({"表": "条件" or ()=>()}).exec(replace_duplicate)
 export class TableSplitter {
 
 }
